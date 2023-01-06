bbfec9fe4a7102deb007fb42f6ba71e355722f35
fhero2030 <jcarloasilo@gmail.com>
2023-01-07 01:25:05 +0800

    extend font and add default colors from figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
         "zeniark-bg": "url('/images/bg.png')",
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#085696",
+        correct: "#4FBD1B",
+        wrong: "#E04E10",
       },
     },
   },
